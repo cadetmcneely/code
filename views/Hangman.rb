7f9d4bae00b9157088cbc82ebe5bc7c47461303d
cadetmcneely <cadetmcneely@gmail.com>
2017-05-20 08:43:06 -0400

    Update Hangman.rb

--- views/Hangman.rb
+++ views/Hangman.rb
@@ -1,39 +1,62 @@
 class Hangman
-	attr_accessor :name, :guess
-	def initilize(password_name)
-			@name = password_name
-		end
-		def charcount
-			name.length
-		end
-		def blank
-			Array.new(charcount,"_")
-		end
-		def correct_letter(letter)
-			name.include?(letter)
-		end
-		def update_guessed(choice)
-			guessed.push(choice)
-		end
-		def correct_index(choice)
-			password=name.split("")
-			p choice
-			p password
-			password.each_with_index do |letter, index_position|
-				if letter == choice
-					correct_blank[index_position] = choice
-				end
-			end
-		end
-		def already_guessed(choice)
-			if guessed.count(choice) == 0
-				true
-			else
-				false
-			end
-		end
-		
-			
-		end
+	attr_accessor :name, :guessed, :correct_blank, :counter
+	def initialize(password_name)
+		@name = password_name.upcase
+		@guessed = []
+		@correct_blank = blank()
+		@counter = 7
+	end
+	def charcount
 
+		name.length
+	end
+	
+	def blank()
+
+		Array.new(charcount, "_").join
+	end
+	def correct_letter(letter)
+
+		name.include?(letter)
+	end
+	def update_guessed(choice)
+
+		guessed.push(choice)
+	end
+	def correct_index(guessletter)
+		password=name.split("")
+		p guessletter
+		p password
+		password.each_with_index do |letter, index_position|
+			p letter
+			if letter == guessletter
+				correct_blank[index_position] = guessletter
+			end
+		end
+	end
+	def available_guess(choice)
+	
+	if	guessed.include?(choice)
+			false
+		else
+			true
+		end
+	end
+	def make_move(choice)
+		if correct_letter(choice) == true
+			correct_index(choice)
+		else
+			@counter -=1
+		end
+	end
+	def loser
+		@counter == 0
+	end
+	def winner
+		if correct_blank.include?("_")
+			false
+		else
+			true
+		end
+end
 end
