d332d6a94351fbe21f7c66a2c0d67c365e6d0e07
cadetmcneely <cadetmcneely@gmail.com>
2017-05-20 08:44:16 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -6,21 +6,17 @@ get '/' do
 	erb :index
 end 
 	post '/player_names' do 
-		player1 = params[:player1]
-		player2 = params[:player2]
-		"Player 1 is #{player1} and Player 2 is #{player2}"
-		redirect '/password?player1=' + player1 + '&player2=' + player2
+		session[:player1] = params[:player1]
+		session[:player2] = params[:player2]
+		#"Player 1 is #{player1} and Player 2 is #{player2}"
+		redirect '/password'
 	end
 	get '/password' do
-		player1 = params[:player1]
-		player2 = params[:player2]
-		erb :password, locals: {p1_name: player1, p2_name: player2}
+		erb :password, locals: {p1_name: session[:player1], p2_name: session[:player2]}
 	end	
 	post '/secretword' do
 		password = params[:word]
 		session[:game] = Hangman.new(password)
-		player1 = params[:player1]
-		player2 = params[:player2]
 			"Player 1 is #{session[:player1]} and player 2 is #{session[:player2]}. your word is #{:password}"	
 		redirect '/guessing'
 	end
